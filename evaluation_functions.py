f67cb2ae483d0011b5b8257c01c4044044435615
Kateryna Meleshenko <yekaterynamel@gmail.com>
2023-09-08 23:22:14 +0200

    df with intermediate number of words was created

--- evaluation_functions.py
+++ evaluation_functions.py
@@ -1,31 +1,74 @@
+# This file is to create additional DataFrame with intermediate numbers of words
 import main
 
 years = main.years
 
 
-def get_all_cleaned_initial_words_nums(years):
-    data = {"total_words": ["All words", "Unique words"]}
-    data_set = set()
+def count_final_words():
+    df = main.df
+    # Count non-zero values in each column
+    non_zero_counts = df.astype(bool).sum(axis=0).to_list()
+    rows_num = len(df)
+
+    return non_zero_counts, rows_num
+
+
+final_words_count, rows_num = count_final_words()
+
+
+def get_all_words_nums(years):
+    data = {
+        "Total_words": [
+            "All words",
+            "Unique words",
+            "Filtered unique words",
+        ]
+    }
+    data_set_cleaned = set()
+    data_set_filtered = set()
+
     for year in years:
         speech_path = main.get_speech_path(year)
         text = main.get_text(speech_path)
         words_total = main.get_words(text, main.speech_regex)
         cleaned_words = main.get_cleaned_words(words_total)
-        words_unique = set(cleaned_words)
-        data_item = {str(year): [len(cleaned_words), len(words_unique)]}
+        filtered_words = main.get_filtered_words(cleaned_words)
+        cleaned_words_unique = set(cleaned_words)
+        filtered_words_unique = set(filtered_words)
+        data_item = {
+            str(year): [
+                len(cleaned_words),
+                len(cleaned_words_unique),
+                len(filtered_words_unique),
+            ]
+        }
         data.update(data_item)
-        data_set.update(words_unique)
+        data_set_cleaned.update(cleaned_words_unique)
+        data_set_filtered.update(filtered_words_unique)
 
-    return data, len(data_set)
-
-
-data, data_set = get_all_cleaned_initial_words_nums(years)
-df_cleaned_initial = main.get_df(data)
-df_cleaned_initial.set_index("total_words", inplace=True)
+    print(data)
+    return data, len(data_set_cleaned), len(data_set_filtered)
 
 
-all_cleaned_initial_words_num = df_cleaned_initial.loc["All words"].sum()
+(
+    data,
+    data_set_cleaned,
+    data_set_filtered,
+) = get_all_words_nums(years)
 
-print(df_cleaned_initial.head())
-print("Total number of cleaned initial words: ", all_cleaned_initial_words_num)
-print("Total number of unique words: ", data_set)
+df_evaluate = main.get_df(data)
+df_evaluate.set_index("Total_words", inplace=True)
+df_evaluate.loc["Lemma words"] = final_words_count
+
+all_words_num = df_evaluate.loc["All words"].sum()
+
+
+print(df_evaluate.head())
+print("Total number of cleaned initial words: ", all_words_num)
+print("Total number of unique cleaned words: ", data_set_cleaned)
+print("Total number of unique cleaned and filtered words: ", data_set_filtered)
+print(
+    "Total number of words after lemmatization and all fixes (final): ",
+    df_evaluate.loc["Lemma words"].sum(),
+)
+print("Number of final tokens: ", rows_num)
--- evaluation_visualization.py
+++ evaluation_visualization.py
@@ -8,76 +8,76 @@ import evaluation_functions
 df_cleaned_initial = evaluation_functions.df_cleaned_initial
 
 
-def run_function(df, function, title="Words proportion for each year"):
-    function(df, title)
+def run_function(df, function, title, bar_1, bar_2):
+    function(df, title, bar_1, bar_2)
     return
 
 
-def visualize_parts_venn(df, title):
-    # Create subplots
-    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
-    axs = axs.ravel()
+# def visualize_parts_venn(df, title):
+#     # Create subplots
+#     fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+#     axs = axs.ravel()
 
-    # Loop through years and create Venn diagrams
-    for i, year in enumerate(df.columns):
-        total_words = set(range(df.loc[df.index[0], year]))
-        unique_words = set(range(df.loc[df.index[1], year]))
+#     # Loop through years and create Venn diagrams
+#     for i, year in enumerate(df.columns):
+#         total_words = set(range(df.loc[df.index[0], year]))
+#         unique_words = set(range(df.loc[df.index[1], year]))
 
-        venn2(
-            [total_words, unique_words],
-            set_labels=(df.index[0], df.index[1]),
-            ax=axs[i],
-        )
+#         venn2(
+#             [total_words, unique_words],
+#             set_labels=(df.index[0], df.index[1]),
+#             ax=axs[i],
+#         )
 
-        axs[i].set_title(f"Year {year}")
-    # Add a title for the entire plot
-    plt.suptitle(title)
+#         axs[i].set_title(f"Year {year}")
+#     # Add a title for the entire plot
+#     plt.suptitle(title)
 
-    # Display the plot
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-    plt.show()
-
-
-def visualize_parts_circles(df, title):
-    # Create subplots
-    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
-    axs = axs.ravel()
-
-    # Loop through years and create pie charts
-    for i, year in enumerate(df.columns):
-        total_words = df.loc[df.index[0], year]
-        unique_words = df.loc[df.index[1], year]
-        common_words = total_words - unique_words
-
-        labels = df.index[0], df.index[1]
-        sizes = [common_words, unique_words]
-        colors = ["blue", "orange"]
-
-        axs[i].pie(
-            sizes, labels=labels, colors=colors, autopct="%1.1f%%", startangle=140
-        )
-        axs[i].axis(
-            "equal"
-        )  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-        axs[i].set_title(f"Year {year}")
-
-    # Add a title for the entire plot
-    plt.suptitle(title)
-
-    # Display the plot
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-    plt.show()
+#     # Display the plot
+#     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+#     plt.show()
 
 
-def visualize_parts_barchart(df, title):
+# def visualize_parts_circles(df, title):
+#     # Create subplots
+#     fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+#     axs = axs.ravel()
+
+#     # Loop through years and create pie charts
+#     for i, year in enumerate(df.columns):
+#         total_words = df.loc[df.index[0], year]
+#         unique_words = df.loc[df.index[1], year]
+#         common_words = total_words - unique_words
+
+#         labels = df.index[0], df.index[1]
+#         sizes = [common_words, unique_words]
+#         colors = ["blue", "orange"]
+
+#         axs[i].pie(
+#             sizes, labels=labels, colors=colors, autopct="%1.1f%%", startangle=140
+#         )
+#         axs[i].axis(
+#             "equal"
+#         )  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+#         axs[i].set_title(f"Year {year}")
+
+#     # Add a title for the entire plot
+#     plt.suptitle(title)
+
+#     # Display the plot
+#     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+#     plt.show()
+
+
+def visualize_parts_barchart(df, title, bar_1, bar_2):
     # Get years from the DataFrame columns
     years = df.columns
 
     # Get word counts from the DataFrame
     word_counts = {
-        "All words": df.loc["All words"],
-        "Unique words": df.loc["Unique words"],
+        bar_1: df.loc[bar_1],
+        bar_2: df.loc[bar_2],
     }
 
     x = np.arange(len(years))  # the label locations
@@ -91,8 +91,8 @@ def visualize_parts_barchart(df, title):
         rects = ax.bar(x + offset, counts, width, label=attribute)
         ax.bar_label(rects, padding=3)
 
-        if attribute == "Unique words":
-            total_words_counts = df.loc["All words"]  # Total words counts for each year
+        if attribute == bar_2:
+            total_words_counts = df.loc[bar_1]  # Total words counts for each year
             percentages = [
                 count / total * 100 for count, total in zip(counts, total_words_counts)
             ]
@@ -122,4 +122,24 @@ def visualize_parts_barchart(df, title):
     plt.show()
 
 
-run_function(df_cleaned_initial, visualize_parts_barchart)
+run_function(
+    df_cleaned_initial,
+    visualize_parts_barchart,
+    "Words proportion for each year",
+    "All words",
+    "Unique words",
+)
+run_function(
+    df_cleaned_initial,
+    visualize_parts_barchart,
+    "Words proportion for each year",
+    "All words",
+    "Filtered unique words",
+)
+run_function(
+    df_cleaned_initial,
+    visualize_parts_barchart,
+    "Words proportion for each year",
+    "Unique words",
+    "Filtered unique words",
+)
